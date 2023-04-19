0f03176909defed178b020ffe09a621b38aebb89
yungbrave <SergeyIlinR@gmail.com>
2023-04-19 16:47:09 +0300

    Added exception for incorrect operand

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -1,117 +1,122 @@
 import java.util.Objects;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws NoNegativeInArabic, InvalidOperandFormat, WrongNumbers, NotUpToTheTask, NotMathOperation, NoZeroInArabic {
-        System.out.println(calc("X / III"));
+    public static void main(String[] args) throws NoNegativeInRomanExceprion, InvalidOperanFormatException, WrongNumbersException, NotUpToTheTaskException, NotMathOperationException, NoZeroInRomanException {
+        System.out.println(calc());
     }
-    public static String calc(String input) throws InvalidOperandFormat, NotMathOperation, WrongNumbers, NoNegativeInArabic, NotUpToTheTask, NoZeroInArabic {
+    public static String calc() throws InvalidOperanFormatException, NotMathOperationException, WrongNumbersException, NoNegativeInRomanExceprion, NotUpToTheTaskException, NoZeroInRomanException {
         String term1 = "";
         String term2 = "";
-        String[] arabic = new String[] {"I","II","III","IV","V","VI","VII","VIII","IX","X"};
+        String[] romanNumeric = new String[] {"I","II","III","IV","V","VI","VII","VIII","IX","X"};
         String result;
-        String[] newString = input.split(" ");
-        if (newString.length < 3 || (!Objects.equals(newString[1], "+") && !Objects.equals(newString[1], "-") && !Objects.equals(newString[1], "*") && !Objects.equals(newString[1], "/"))) {
-            throw new NotMathOperation("т.к. не является математической операцией");
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        String[] splitedExpression = input.split(" ");
+        if (splitedExpression.length < 3 || (!Objects.equals(splitedExpression[1], "+") && !Objects.equals(splitedExpression[1], "-") && !Objects.equals(splitedExpression[1], "*") && !Objects.equals(splitedExpression[1], "/")))
+            throw new NotMathOperationException("т.к. не является математической операцией");
+        else if (splitedExpression.length > 3) {
+            throw new NotUpToTheTaskException("т.к. формат не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
         }
-        else if (newString.length > 3) {
-            throw new NotUpToTheTask("т.к. формат не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
-        }
-        // поиск арабских чисел
-        for (String value : arabic) {
-            if (value.equals(newString[0])) {
+        // поиск римских чисел
+        for (String value : romanNumeric) {
+            if (value.equals(splitedExpression[0])) {
                 term1 = value;
             }
         }
-        for (String s : arabic) {
-            if (s.equals(newString[2])) {
-                term2 = s;
+        for (String value : romanNumeric) {
+            if (value.equals(splitedExpression[2])) {
+                term2 = value;
             }
         }
 
         if (term1.equals("") && term2.equals("")) {
-            if (((Integer.parseInt(newString[0]) < 11) && (Integer.parseInt(newString[0])>0)) && ((Integer.parseInt(newString[2]) < 11) && (Integer.parseInt(newString[2])>0))) {
-                switch (newString[1]) {
+            if (((Integer.parseInt(splitedExpression[0]) < 11) && (Integer.parseInt(splitedExpression[0])>0)) && ((Integer.parseInt(splitedExpression[2]) < 11) && (Integer.parseInt(splitedExpression[2])>0))) {
+                switch (splitedExpression[1]) {
                     case "+":
-                        result = String.valueOf(Integer.parseInt(newString[0]) + Integer.parseInt(newString[2]));
+                        result = String.valueOf(Integer.parseInt(splitedExpression[0]) + Integer.parseInt(splitedExpression[2]));
                         break;
                     case "-":
-                        result = String.valueOf(Integer.parseInt(newString[0]) - Integer.parseInt(newString[2]));
+                        result = String.valueOf(Integer.parseInt(splitedExpression[0]) - Integer.parseInt(splitedExpression[2]));
                         break;
                     case "*":
-                        result = String.valueOf(Integer.parseInt(newString[0]) * Integer.parseInt(newString[2]));
+                        result = String.valueOf(Integer.parseInt(splitedExpression[0]) * Integer.parseInt(splitedExpression[2]));
                         break;
                     case "/":
-                        result = String.valueOf(Integer.parseInt(newString[0]) / Integer.parseInt(newString[2]));
+                        result = String.valueOf(Integer.parseInt(splitedExpression[0]) / Integer.parseInt(splitedExpression[2]));
                         break;
-                    default: throw new NotMathOperation ("т.к. формат не удовлетворяет заданию, неверный оператор");
+                    default: throw new NotMathOperationException("т.к. формат не удовлетворяет заданию, неверный оператор");
                 }
                 return result;
             } else {
-                throw new WrongNumbers("Введены числа, выходящие за пределы от 1 до 10");
+                throw new WrongNumbersException("Введены числа, выходящие за пределы от 1 до 10");
             }
         } else if (term1.equals("") || term2.equals("")) {
-            throw new InvalidOperandFormat ("т.к. используются одноверменно разные системы счисления");
+            if (ChangeNumericSystem.isNumeric(splitedExpression[0]) || ChangeNumericSystem.isNumeric(splitedExpression[2])) {
+                throw new InvalidOperanFormatException("т.к. используются одноверменно разные системы счисления");
+            } else {
+                throw new NotUpToTheTaskException("Некорректно введны операнды");
+            }
         } else {
-            switch (newString[1]) {
+            switch (splitedExpression[1]) {
                 case "+":
-                    result = ArabicToRoman.getArabic(ArabicToRoman.getRoman(newString[0]) + ArabicToRoman.getRoman(newString[2]));
+                    result = ChangeNumericSystem.getArabic(ChangeNumericSystem.getRoman(splitedExpression[0]) + ChangeNumericSystem.getRoman(splitedExpression[2]));
                     break;
                 case "-":
-                    if ((ArabicToRoman.getRoman(newString[0]) - ArabicToRoman.getRoman(newString[2]) < 1)) {
-                        throw new NoZeroInArabic ("т.к. в римской системе нет нуля");
-                    } else if ((ArabicToRoman.getRoman(newString[0]) - ArabicToRoman.getRoman(newString[2]) < 0)) {
-                        throw new NoNegativeInArabic("т.к. в римской системе нет отрицательных чисел");
+                    if ((ChangeNumericSystem.getRoman(splitedExpression[0]) - ChangeNumericSystem.getRoman(splitedExpression[2]) < 1)) {
+                        throw new NoZeroInRomanException("т.к. в римской системе нет нуля");
+                    } else if ((ChangeNumericSystem.getRoman(splitedExpression[0]) - ChangeNumericSystem.getRoman(splitedExpression[2]) < 0)) {
+                        throw new NoNegativeInRomanExceprion("т.к. в римской системе нет отрицательных чисел");
                     } else {
-                        result = ArabicToRoman.getArabic(ArabicToRoman.getRoman(newString[0]) - ArabicToRoman.getRoman(newString[2]));
+                        result = ChangeNumericSystem.getArabic(ChangeNumericSystem.getRoman(splitedExpression[0]) - ChangeNumericSystem.getRoman(splitedExpression[2]));
                     }
                     break;
                 case "*":
-                    result = ArabicToRoman.getArabic(ArabicToRoman.getRoman(newString[0]) * ArabicToRoman.getRoman(newString[2]));
+                    result = ChangeNumericSystem.getArabic(ChangeNumericSystem.getRoman(splitedExpression[0]) * ChangeNumericSystem.getRoman(splitedExpression[2]));
                     break;
                 case "/":
-                    result = ArabicToRoman.getArabic(ArabicToRoman.getRoman(newString[0]) / ArabicToRoman.getRoman(newString[2]));
+                    result = ChangeNumericSystem.getArabic(ChangeNumericSystem.getRoman(splitedExpression[0]) / ChangeNumericSystem.getRoman(splitedExpression[2]));
                     break;
-                default: throw new NotMathOperation ("т.к. формат не удовлетворяет заданию, неверный оператор");
+                default: throw new NotMathOperationException("т.к. формат не удовлетворяет заданию, неверный оператор");
             }
             return result;
         }
-        //добавить return
     }
 }
 //Exceptions
-class NoZeroInArabic extends Exception {
-    NoZeroInArabic(String description) {
+class NoZeroInRomanException extends Exception {
+    NoZeroInRomanException(String description) {
         super(description);
     }
 }
-class NotUpToTheTask extends Exception{
-    NotUpToTheTask (String description) {
+class NotUpToTheTaskException extends Exception{
+    NotUpToTheTaskException(String description) {
         super(description);
     }
 }
-class InvalidOperandFormat extends Exception{
-    public InvalidOperandFormat (String description) {
+class InvalidOperanFormatException extends Exception{
+    public InvalidOperanFormatException(String description) {
         super(description);
     }
 }
-class NoNegativeInArabic extends Exception{
-    NoNegativeInArabic (String description) {
+class NoNegativeInRomanExceprion extends Exception{
+    NoNegativeInRomanExceprion(String description) {
         super(description);
     }
 }
-class WrongNumbers extends Exception{
-    public WrongNumbers (String description) {
+class WrongNumbersException extends Exception{
+    public WrongNumbersException(String description) {
         super(description);
     }
 }
-class NotMathOperation extends Exception{
-    NotMathOperation (String description) {
+class NotMathOperationException extends Exception{
+    NotMathOperationException(String description) {
         super(description);
     }
 }
 //Методы перевода чисел из римской в арабскую СИ и обратно
-class ArabicToRoman {
-    public static String[] arabic = new String[] {
+class ChangeNumericSystem {
+    public static String[] roman = new String[] {
             "I","II","III","IV","V","VI","VII","VIII","IX","X",
             "XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX",
             "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX",
@@ -123,23 +128,31 @@ class ArabicToRoman {
             "LXXXI","LXXXII","LXXXIII","LXXXIV","LXXXV","LXXXVI","LXXXVII","LXXXVIII","LXXXIX","XC",
             "XCI","XCII","XCIII","XCIV","XCV","XCVI","XCVII","XCVIII","XCIX","C"
     };
-    public static int getRoman (String arabic1) {
+    public static int getRoman(String roman1) {
         int term = 0;
-        for (int i = 0; i < arabic.length; i++) {
-            if (arabic1.equals(arabic[i])) {
+        for (int i = 0; i < roman.length; i++) {
+            if (roman1.equals(roman[i])) {
                 term = i+1;
             }
 
         }
         return term;
     }
-    public static String getArabic (int result) {
-        String resultArabic = null;
-        for (int i = 0; i < arabic.length; i++) {
+    public static String getArabic(int result) {
+        String resultRoman = null;
+        for (int i = 0; i < roman.length; i++) {
             if (result == i+1) {
-                resultArabic = arabic [i];
+                resultRoman = roman[i];
             }
         }
-        return resultArabic;
+        return resultRoman;
+    }
+    public static boolean isNumeric(String strNum) {
+        try {
+            Integer.parseInt(strNum);
+            } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
     }
 }
